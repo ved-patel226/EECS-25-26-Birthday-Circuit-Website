a46d537fc9c10111374c50376c823503837b153b
Ved Patel <talk2ved11@gmail.com>
2025-11-13 20:44:48 -0500

    added component pictrues

--- src/components/Components.tsx
+++ src/components/Components.tsx
@@ -7,30 +7,35 @@ export default function Components() {
       name: "Arduino Nano",
       description:
         "The Ardiuno acts as the PLD (Programmable Logic Device) to change 3 bit input -> outputs for segments. We can get these boolean algebra expressions by Karnaugh maps",
+      pic: "imgs/ardiuno.jpg",
     },
     {
       id: 2,
       name: "SSD",
       description:
         "The seven-segment display is used to show my birthday. We use two of them.",
+      pic: "imgs/SSD.jpg",
     },
     {
       id: 3,
       name: "74LS93N",
       description:
         "4-bit binary counter IC chip. This chip takes a clock input from the timer 555 and counts up with 4 bits.",
+      pic: "imgs/74LS93N.png",
     },
     {
       id: 4,
       name: "Timer 555",
       description:
-        "The 555 timer generates a clock pulse that drives the 74LS93N counter. The pulse frequency determines how fast the display cycles through my birthday.",
+        "The 555 timer generates a clock pulse for the 74LS93N counter. The pulse determines how fast the display cycles through my birthday.",
+      pic: "imgs/555.jpg",
     },
     {
       id: 5,
       name: "Potentiometer",
       description:
-        "A variable resistor used to adjust the resistance in the 555 timer circuit. Changing the resistance alters the timer's frequency, which controls how fast the display cycles through the birthday digits.",
+        "A variable resistor used to adjust the resistance in the 555 timer circuit. Changing the resistance controls how the speed of the output 555 pulse.",
+      pic: "imgs/potentiometer.jpg",
     },
   ];
 
@@ -44,10 +49,7 @@ export default function Components() {
       <ul className={styles.component_list}>
         {components.map((component) => (
           <li key={component.id} className={styles.component_item}>
-            <img
-              src={`https://picsum.photos/seed/${component.id}/1000/400`}
-              alt={component.name}
-            />
+            <img src={component.pic} alt={component.name} />
             <div className={styles.component_overlay}>
               <h3>{component.name}</h3>
               <p>{component.description}</p>
