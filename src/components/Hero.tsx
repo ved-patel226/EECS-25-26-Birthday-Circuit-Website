54ea5842097fbe5888c028b1ff74c7c673066664
Ved Patel <talk2ved11@gmail.com>
2025-11-13 21:18:29 -0500

    added github link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,14 +6,26 @@ export default function Hero() {
       <h1>Birthday Circuit</h1>
       <p>The EECS final exploratory project. Created by Ved Patel.</p>
 
-      <div className="btn">
-        <a
-          href="https://docs.google.com/document/d/1JL9dlh84g1gIEW48HJqDEjuBlnhaI8h-riEJypo-m2g/edit?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ⏰ View Version History
-        </a>
+      <div className="btns">
+        <div className="btn">
+          <a
+            href="https://docs.google.com/document/d/1JL9dlh84g1gIEW48HJqDEjuBlnhaI8h-riEJypo-m2g/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ⏰ View Version History
+          </a>
+        </div>
+
+        <div className="btn">
+          <a
+            href="https://github.com/ved-patel226/EECS-25-26-Birthday-Circuit-Website"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            🐙 View Github
+          </a>
+        </div>
       </div>
     </div>
   );
